70ff192fbb4d6ac5f7c602ccfef6a83eb063bb87
Sim Justin <1sim.justin@gmail.com>
2024-03-30 17:43:18 +0800

    Fix above

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -6,8 +6,8 @@ cd "${0%/*}"
 cd ..
 ./gradlew clean shadowJar
 
-rm -rf ./data/
 cd text-ui-test
+rm -rf ./data/
 
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
