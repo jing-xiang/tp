a56f15efdc4249c1f6ce4a974b331dedb4687ca2
Sim Justin <1sim.justin@gmail.com>
2024-04-13 01:31:15 +0800

    Update .sh file

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -10,6 +10,11 @@ cd text-ui-test
 rm -rf ./data/
 
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input/input1.txt > actual_output/ACTUAL1.TXT
+java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input/input2.txt > actual_output/ACTUAL2.TXT
+java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input/input3.txt > actual_output/ACTUAL3.TXT
+java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input/input4.txt > actual_output/ACTUAL4.TXT
+
+ERROR_COUNT=0
 
 cp expected_output/EXPECTED1.TXT expected_output/EXPECTED1-UNIX.TXT
 dos2unix expected_output/EXPECTED1-UNIX.TXT actual_output/ACTUAL1.TXT
@@ -17,10 +22,9 @@ diff expected_output/EXPECTED1-UNIX.TXT actual_output/ACTUAL1.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
-    exit 0
 else
+    ERROR_COUNT=$((ERROR_COUNT+1))
     echo "Test failed!"
-    exit 1
 fi
 
 cp expected_output/EXPECTED2.TXT expected_output/EXPECTED2-UNIX.TXT
@@ -29,10 +33,9 @@ diff expected_output/EXPECTED2-UNIX.TXT actual_output/ACTUAL2.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
-    exit 0
 else
+    ERROR_COUNT=$((ERROR_COUNT+1))
     echo "Test failed!"
-    exit 1
 fi
 
 cp expected_output/EXPECTED3.TXT expected_output/EXPECTED3-UNIX.TXT
@@ -41,10 +44,9 @@ diff expected_output/EXPECTED3-UNIX.TXT actual_output/ACTUAL3.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
-    exit 0
 else
+    ERROR_COUNT=$((ERROR_COUNT+1))
     echo "Test failed!"
-    exit 1
 fi
 
 cp expected_output/EXPECTED4.TXT expected_output/EXPECTED4-UNIX.TXT
@@ -53,8 +55,9 @@ diff expected_output/EXPECTED4-UNIX.TXT actual_output/ACTUAL4.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
-    exit 0
 else
+    ERROR_COUNT=$((ERROR_COUNT+1))
     echo "Test failed!"
-    exit 1
 fi
+
+exit $ERROR_COUNT
