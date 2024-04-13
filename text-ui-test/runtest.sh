c75a5f925074cddb24c0f070792bdf9da4b53e67
1simjustin <1sim.justin@gmail.com>
2024-04-13 17:36:26 +0800

    Update .sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -11,6 +11,7 @@ cd ..
 
 # Change directory to text-ui-test
 cd text-ui-test
+rm -rf ./data/
 
 # Function to check test result
 check_test() {
@@ -24,7 +25,7 @@ check_test() {
     # Run the test and generate actual output
     java -jar "$(find ../build/libs/ -mindepth 1 -print -quit)" < "$input_file" > "$actual_output"
 
-    cp "$expected_output" "$expected_output-UNIX.TXT"
+    cp "$expected_output.TXT" "$expected_output-UNIX.TXT"
     dos2unix "$expected_output-UNIX.TXT" "$actual_output"
     diff "$expected_output-UNIX.TXT" "$actual_output"
     
@@ -42,18 +43,18 @@ ERROR_COUNT=0
 FAILED_TESTS=""
 
 # Run tests
-check_test "input/input1.txt" "expected_output/EXPECTED1.TXT" "actual_output/ACTUAL1.TXT" "1" ERROR_COUNT FAILED_TESTS
-check_test "input/input2.txt" "expected_output/EXPECTED2.TXT" "actual_output/ACTUAL2.TXT" "2" ERROR_COUNT FAILED_TESTS
-check_test "input/input3.txt" "expected_output/EXPECTED3.TXT" "actual_output/ACTUAL3.TXT" "3" ERROR_COUNT FAILED_TESTS
-check_test "input/input4.txt" "expected_output/EXPECTED4.TXT" "actual_output/ACTUAL4.TXT" "4" ERROR_COUNT FAILED_TESTS
-check_test "input/input5A.txt" "expected_output/EXPECTED5A.TXT" "actual_output/ACTUAL5A.TXT" "5A" ERROR_COUNT FAILED_TESTS
-check_test "input/input5B.txt" "expected_output/EXPECTED5B.TXT" "actual_output/ACTUAL5B.TXT" "5B" ERROR_COUNT FAILED_TESTS
+check_test "input/input5B.txt" "expected_output/EXPECTED5B" "actual_output/ACTUAL5B.TXT" "5B" ERROR_COUNT FAILED_TESTS
+check_test "input/input1.txt" "expected_output/EXPECTED1" "actual_output/ACTUAL1.TXT" "1" ERROR_COUNT FAILED_TESTS
+check_test "input/input2.txt" "expected_output/EXPECTED2" "actual_output/ACTUAL2.TXT" "2" ERROR_COUNT FAILED_TESTS
+check_test "input/input3.txt" "expected_output/EXPECTED3" "actual_output/ACTUAL3.TXT" "3" ERROR_COUNT FAILED_TESTS
+check_test "input/input4.txt" "expected_output/EXPECTED4" "actual_output/ACTUAL4.TXT" "4" ERROR_COUNT FAILED_TESTS
+check_test "input/input5A.txt" "expected_output/EXPECTED5A" "actual_output/ACTUAL5A.TXT" "5A" ERROR_COUNT FAILED_TESTS
 
 # Output test results
 if [ $ERROR_COUNT -eq 0 ]; then
     echo "All tests passed!"
 else
-    echo "$ERROR_COUNT tests failed: $FAILED_TESTS"
+    echo "$ERROR_COUNT tests failed:$FAILED_TESTS"
 fi
 
 # Exit with error count
