4d49df37c08f59bc249c2f8576c395289a5c5a4a
Sim Justin <1sim.justin@gmail.com>
2024-04-13 16:10:30 +0800

    Update shell script

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -17,12 +17,20 @@ check_test() {
     local expected_output="$1"
     local actual_output="$2"
     local test_name="$3"
+    local error_count_ref="$4"
+    local failed_tests_ref="$5"
     
     cp "$expected_output" "$expected_output-UNIX.TXT"
     dos2unix "$expected_output-UNIX.TXT" "$actual_output"
-    diff "$expected_output-UNIX.TXT" "$actual_output"
+    diff "$expected_output-UNIX.TXT" "$actual_output" > /dev/null
     
-    return $?
+    if [ $? -eq 0 ]; then
+        echo "Test $test_name passed!"
+    else
+        echo "Test $test_name failed!"
+        eval "$error_count_ref+=1"
+        eval "$failed_tests_ref+=\" $test_name\""
+    fi
 }
 
 # Initialize error count variable
@@ -32,41 +40,12 @@ ERROR_COUNT=0
 FAILED_TESTS=""
 
 # Run tests
-check_test "expected_output/EXPECTED1.TXT" "actual_output/ACTUAL1.TXT" "1"
-if [ $? -ne 0 ]; then
-    ERROR_COUNT=$((ERROR_COUNT + 1))
-    FAILED_TESTS+=" 1"
-fi
-
-check_test "expected_output/EXPECTED2.TXT" "actual_output/ACTUAL2.TXT" "2"
-if [ $? -ne 0 ]; then
-    ERROR_COUNT=$((ERROR_COUNT + 1))
-    FAILED_TESTS+=" 2"
-fi
-
-check_test "expected_output/EXPECTED3.TXT" "actual_output/ACTUAL3.TXT" "3"
-if [ $? -ne 0 ]; then
-    ERROR_COUNT=$((ERROR_COUNT + 1))
-    FAILED_TESTS+=" 3"
-fi
-
-check_test "expected_output/EXPECTED4.TXT" "actual_output/ACTUAL4.TXT" "4"
-if [ $? -ne 0 ]; then
-    ERROR_COUNT=$((ERROR_COUNT + 1))
-    FAILED_TESTS+=" 4"
-fi
-
-check_test "expected_output/EXPECTED5A.TXT" "actual_output/ACTUAL5A.TXT" "5A"
-if [ $? -ne 0 ]; then
-    ERROR_COUNT=$((ERROR_COUNT + 1))
-    FAILED_TESTS+=" 5A"
-fi
-
-check_test "expected_output/EXPECTED5B.TXT" "actual_output/ACTUAL5B.TXT" "5B"
-if [ $? -ne 0 ]; then
-    ERROR_COUNT=$((ERROR_COUNT + 1))
-    FAILED_TESTS+=" 5B"
-fi
+check_test "expected_output/EXPECTED1.TXT" "actual_output/ACTUAL1.TXT" "1" ERROR_COUNT FAILED_TESTS
+check_test "expected_output/EXPECTED2.TXT" "actual_output/ACTUAL2.TXT" "2" ERROR_COUNT FAILED_TESTS
+check_test "expected_output/EXPECTED3.TXT" "actual_output/ACTUAL3.TXT" "3" ERROR_COUNT FAILED_TESTS
+check_test "expected_output/EXPECTED4.TXT" "actual_output/ACTUAL4.TXT" "4" ERROR_COUNT FAILED_TESTS
+check_test "expected_output/EXPECTED5A.TXT" "actual_output/ACTUAL5A.TXT" "5A" ERROR_COUNT FAILED_TESTS
+check_test "expected_output/EXPECTED5B.TXT" "actual_output/ACTUAL5B.TXT" "5B" ERROR_COUNT FAILED_TESTS
 
 # Output test results
 if [ $ERROR_COUNT -eq 0 ]; then
